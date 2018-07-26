c27e24da8ac673e9416632a1081e0d4791dbcf40
days88 <yayahidayatullah@rocketmail.com>
2018-07-26 18:25:05 +0800

    edit

--- exersise-3.js
+++ exersise-3.js
@@ -1,14 +1,15 @@
-var nama  = prompt('Isikan Nama Anda'),
-	peran = prompt("Silahkan Pilih Peran ('Ksatria','Tabib','Penyihir')");
+var nama  = '',
+	peran = '';
+
 if (nama !== '' && peran == 'Ksatria') {
-	alert('"Selamat datang di Dunia Proxytia, ' + nama +'"\n"Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!"');
+	console.log('"Selamat datang di Dunia Proxytia, ' + nama +'"\n"Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!"');
 }else if (nama !== '' && peran == 'Tabib') {
-	alert('"Selamat datang di Dunia Proxytia, ' + nama +'"\n"Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka."');
+	console.log('"Selamat datang di Dunia Proxytia, ' + nama +'"\n"Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka."');
 }else if (nama !== '' && peran == 'Penyihir') {
-	alert('"Selamat datang di Dunia Proxytia, ' + nama +'"\n"Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!"')
+	console.log('"Selamat datang di Dunia Proxytia, ' + nama +'"\n"Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!"')
 }else if (nama !== '' && peran == '') {
-	alert('"Halo ' + nama + ', Pilih peranmu untuk memulai game!"');
+	console.log('"Halo ' + nama + ', Pilih peranmu untuk memulai game!"');
 }else{
-	alert('"Nama harus diisi!"');
+	console.log('"Nama harus diisi!"');
 }
 
