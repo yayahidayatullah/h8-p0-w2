44e1f0bd942a2d4be90a7389b5ccf0f2299311b2
days88 <yayahidayatullah@rocketmail.com>
2018-07-27 10:35:57 +0800

    tugas switch-case

---
+++ exercise-4.js
@@ -0,0 +1,18 @@
+var tanggal = ; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = ; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = ; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+
+switch	(bulan) {
+	case 1  : console.log(tanggal + " januari " + tahun );break;
+	case 2  : console.log(tanggal + " februari " + tahun );break;
+	case 3  : console.log(tanggal + " maret " + tahun );break;
+	case 4  : console.log(tanggal + " april " + tahun );break;
+	case 5  : console.log(tanggal + " mei " + tahun );break;
+	case 6  : console.log(tanggal + " juni " + tahun );break;
+	case 7  : console.log(tanggal + " juli " + tahun );break;
+	case 8  : console.log(tanggal + " agustus " + tahun );break;
+	case 9  : console.log(tanggal + " september " + tahun );break;
+	case 10 : console.log(tanggal + " oktober " + tahun );break;
+	case 11 : console.log(tanggal + " november " + tahun );break;
+	case 12 : console.log(tanggal + " desember " + tahun );break;
+}
