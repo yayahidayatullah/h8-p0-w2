479be39048fa16fc0fe17cfa8deeece98453fb7b
days88 <yayahidayatullah@rocketmail.com>
2018-07-30 21:18:32 +0800

    x dan o

---
+++ exercise-13.js
@@ -0,0 +1,22 @@
+function xo(str) {
+  var hasilX = 0;
+  var hasilO = 0;
+  for (var x = str.length ; x >= 0 ; x-- ){
+    if(str.charAt([x]) === 'x'){
+      hasilX += 1;
+    }else if(str.charAt([x]) === 'o'){
+      hasilO += 1;
+    }
+  }  
+  if( hasilX === hasilO ){
+    return true
+  }else{
+    return false
+  }
+}
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
