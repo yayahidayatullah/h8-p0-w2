9eeb00c40dfead364aceab21ea58f3b24bfc2441
days88 <yayahidayatullah@rocketmail.com>
2018-07-30 23:48:24 +0800

    x dan o

--- exercise-13.js
+++ exercise-13.js
@@ -1,7 +1,7 @@
 function xo(str) {
   var hasilX = 0;
   var hasilO = 0;
-  for (var x = str.length ; x >= 0 ; x-- ){
+  for (var x = str.length-1 ; x >= 0 ; x-- ){
     if(str.charAt([x]) === 'x'){
       hasilX += 1;
     }else if(str.charAt([x]) === 'o'){
